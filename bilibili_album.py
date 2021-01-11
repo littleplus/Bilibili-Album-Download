ee9453c01fb60ac612e3e7452377755d569db43b
littleplus <littleplus@users.noreply.github.com>
2021-01-12 00:23:20 +0800

    Add HTTP headers to bypass bilibili limitation

--- bilibili_album.py
+++ bilibili_album.py
@@ -3,12 +3,16 @@ import requests,os,sys,math
 
 basicApiUrl='https://api.vc.bilibili.com/link_draw/v1/doc/upload_count?uid='
 apiUrl='https://api.vc.bilibili.com/link_draw/v1/doc/doc_list?page_size=30&biz=all&uid='
+headers={
+	'Referer':'https://space.bilibili.com/',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
+}
 
 # Get the amount of all draws
 # If error return 0
 def getTotalDraw(bid):
 	try:
-		req=requests.get(basicApiUrl+bid)
+		req=requests.get(basicApiUrl+bid,headers=headers)
 		rspJson = req.json()
 	except:
 		return 0
@@ -26,7 +30,7 @@ def downloadDrawList(bid,page):
 	url = url+'&page_num='+str(page)
 	
 	try:
-		req=requests.get(url,timeout=5)
+		req=requests.get(url,timeout=5,headers=headers)
 		rspJson = req.json()
 		
 		# Get all items in a range
@@ -68,7 +72,7 @@ def downloadDraw(bid,did,urls):
 				continue
 			print('Downloading '+did+' '+u)
 			# Download single image
-			req = requests.get(u,timeout=20)			
+			req = requests.get(u,timeout=20,headers=headers)			
 			# Create image file
 			with open('./'+bid+'/'+fileName,'wb') as f:
 				f.write(req.content)
